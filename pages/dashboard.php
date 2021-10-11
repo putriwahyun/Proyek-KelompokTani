bf9ff1a8897306421017f8edeb947e49d9d5ec02
greseliaamanda <greseliamanda16@gmail.com>
2021-10-11 23:20:43 +0700

    sidebar first commit

---
+++ pages/dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <div id="content" class="p-4 p-md-5 pt-5">
+      <br><br><br><br><br><br><br><br>
+        <!--alert-->
+        <div class="alert alert-dark" role="alert">
+          <p>Welcome!</p>
+          <hr>
+          <p class="mb-0">Hope you enjoyed.</p>
+        </div>
+    </div>
+  </body>
+</html>
