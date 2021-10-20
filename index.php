ff500bae640eec1242dd5ac7729b74b8861c0707
greseliaamanda <greseliamanda16@gmail.com>
2021-10-20 15:44:53 +0700

    beberapa fitur

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="css/style.css">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- import boxicons -->
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <!-- datatables -->
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css"> -->
+    <link rel="stylesheet" type="text/css" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIDEBAR</title>
 </head>
@@ -26,28 +32,28 @@
                 <span class="tooltip">Dashboard</span>
             </li>
             <li>
-                <a href="#">
+                <a href="index.php?p=kelompok-tani">
                     <i class='bx bxs-group'></i>
                     <span class="links_name">Data Kelompok Tani</span>
                 </a>
                 <span class="tooltip">Data Kelompok Tani</span>
             </li>
             <li>
-                <a href="#">
+                <a href="index.php?p=anggota">
                     <i class='bx bxs-user'></i>
                     <span class="links_name">Data Anggota<br> Kelompok Tani</span>
                 </a>
                 <span class="tooltip">Data Anggota<br> Kelompok Tani</span>
             </li>
             <li>
-                <a href="#">
+                <a href="index.php?p=bantuan">
                     <i class='bx bxs-folder'></i>
                     <span class="links_name">Data Bantuan</span>
                 </a>
                 <span class="tooltip">Data Bantuan</span>
             </li>
             <li>
-                <a href="#">
+                <a href="index.php?p=penyuluhan">
                     <i class='bx bxs-time'></i>
                     <span class="links_name">Data Penyuluhan</span>
                 </a>
@@ -83,5 +89,13 @@
     </section>
 
     <script src="js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        } );
+    </script>
 </body>
 </html>
---
+++ pages/kelompok-tani.php
@@ -0,0 +1,50 @@
+<body>
+    <div class="main-content">
+        <header>
+            <h4>
+                Data Kelompok Tani
+            </h4>
+            <div class="user-wrapper">
+                <img src="bxs-user-circle.svg" width="40px" height="40px" alt="">
+                <div>
+                <h6>Admin</h6>
+                </div>
+            </div>
+        </header>
+
+        <div class="container">
+            <div class="tambah">
+                <button type="button" class="btn btn-primary"><i class='bx bxs-user-plus'></i>Tambah Kelompok</button>
+            </div>
+            <table id="example" class="table table-striped " style="width:100%">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama Kelompok Tani</th>
+                        <th scope="col">Nama Ketua</th>
+                        <th scope="col">Alamat</th>
+                        <th scope="col">Kelas</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="col">1</td>
+                        <td scope="col">Kelompok Tani Makmur</td>
+                        <td scope="col">Makmur</td>
+                        <td scope="col">Dsn. Gabus Ds. Gabus</th>
+                        <td scope="col">Pemula</td>
+                        <td scope="col">
+                            <button type="button" class="btn btn-warning"><i class='bx bxs-edit'></i></button>
+                            <button type="button" class="btn btn-danger"><i class='bx bxs-trash-alt'></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script>$(document).ready(function() {
+        $('#example').DataTable();
+        } );
+    </script>
+</body>
