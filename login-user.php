d63cae7840d51147cc8c5a80d0e0aa7422f1801f
== <==>
2021-10-20 16:03:22 +0700

    login,tambah anggota

---
+++ login-user.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+    <link rel="stylesheet" href="style_login.css">
+</head>
+<body>
+    <div style="color: black; position: absolute; left: 330px; top: 110px; font-size: 40px; font-weight: bold;">
+        GAPOKTAN KECAMATAN JOMBANG
+    </div>
+    <div class="container">
+        <form method="post" action="index.php?p=dashboard">
+            <div style="height: 35px;"></div>
+            <input type="text" name="user" placeholder="Masukkan Username"><br>
+            <input type="password" name="pass" placeholder="Masukkan Password"><br>
+            <div class="vertical-center">
+                <button type="submit" name="submit">Masuk</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ pages/tambah-anggota.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+	<title>Tambah Anggota</title>
+	<style type="text/css" media="screen">
+		table {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 11px;}
+		input {font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 11px;height: 20px;}
+	</style>
+</head>
+<body>
+<div style="border:0; padding:10px; width:auto; height:auto;">
+<form action="action-input-data.php" method="POST" name="form-input-data">
+    <div class="latar" style="width: auto; background-color: teal; padding: 5px; border-radius: 5px; text-align: center;">
+        <h3 style="color: white;">Tambah Data Anggota Kelompok Tani</h3>
+    </div>
+	<table width="auto" border="0" align="center" cellpadding="0" cellspacing="0">
+		<tr height="20">
+				<td width="10%"> </td>
+				<td width="25%"> </td>
+		</tr>
+
+        <tr height="46">
+			<td> </td>
+			<td>NIK</td>
+			<td><input type="text" name="nik" size="40" maxlength="30" /></td>
+		</tr>
+		<tr height="46">
+			<td> </td>
+			<td>Nama Anggota</td>
+			<td><input type="text" name="nama" size="40" maxlength="30" /></td>
+		</tr>
+		<tr height="46">
+			<td> </td>
+			<td>Alamat</td>
+			<td><input type="text" name="alamat" size="40" maxlength="30" /></td>
+		</tr>
+        <tr height="46">
+			<td> </td>
+			<td>No SPPT</td>
+			<td><input type="text" name="nosppt" size="40" maxlength="30" /></td>
+		</tr>
+        <tr height="46">
+			<td> </td>
+			<td>Luas SPPT</td>
+			<td><input type="text" name="luassppt" size="40" maxlength="30" /></td>
+		</tr>
+        <tr height="46">
+			<td> </td>
+			<td>Nama Ibu</td>
+			<td><input type="text" name="namaibu" size="40" maxlength="30" /></td>
+		</tr>
+        <tr height="46">
+			<td> </td>
+			<td>Koordinat</td>
+			<td><input type="text" name="koordinat" size="40" maxlength="30" /></td>
+		</tr>
+        <tr height="46">
+			<td> </td>
+			<td>No Telepon</td>
+			<td><input type="text" name="telepon" size="40" maxlength="30" /></td>
+		</tr>
+        <tr height="46">
+			<td> </td>
+			<td>Komoditas</td>
+			<td><input type="text" name="komoditas" size="40" maxlength="30" /></td>
+		</tr>
+        <tr height="46">
+			<td> </td>
+			<td>Luas Tanam</td>
+			<td><input type="text" name="luastanam" size="40" maxlength="30" /></td>
+		</tr>
+		<tr height="46" width="100">
+			<td> </td>
+			<td> </td>
+			<td><input type="submit" name="Submit" value="Submit">   
+				<input type="reset" name="reset" value="Kembali"></td>
+		</tr>
+	</table>
+</form>
+</div>
+</body>
+</html>
